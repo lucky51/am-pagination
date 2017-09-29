0e1e62aaf4e17e5caf612ca27be5a1c067d931fe
lucky51 <941841706@qq.com>
2017-09-29 13:25:56 +0800

    change repository

---
+++ am-pagination/gulpfile.js
@@ -0,0 +1,19 @@
+﻿
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+     rename = require('gulp-rename'),
+      notify = require('gulp-notify'),
+      uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+gulp.task('scripts', function () {
+    return gulp.src(['./../am-pagination.js'])
+      .pipe(jshint('init.jshintrc'))
+      .pipe(jshint.reporter('default'))
+      .pipe(concat('am-pagination.js'))
+      .pipe(gulp.dest('dist/js'))
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/js'))
+      .pipe(notify({ message: 'Scripts task complete' }));
+});
